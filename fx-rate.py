34d1899bca4da4d1f81c74281c3173e13f3113f5
Bill Ying <bill_ying@hotmail.com>
2021-04-02 12:06:29 -0400

    Update the private functions

--- fx-rate.py
+++ fx-rate.py
@@ -6,62 +6,66 @@ import requests
 
 BOC_API_LINK = 'https://www.bankofcanada.ca/valet//observations/'
 
+
 def main(*args, **kwargs):
+    url = BOC_API_LINK + ('FXCADUSD' if args[0] == 'c' else 'FXUSDCAD')
+    bank_of_canada_response = requests.get(url).json()
     month_ending = kwargs['month_ending']
-    single_date = kwargs['single_date']
-
-    if args[0] == 'c':
-        url = BOC_API_LINK + 'FXCADUSD'
-    else:
-        url = BOC_API_LINK + 'FXUSDCAD'
-
-    exchange_response = requests.get(url).json()
-    first_available_date = exchange_response['observations'][0]['d']
-    first_available_year = first_available_date.split('-')[0]
-    last_available_date = exchange_response['observations'][-1]['d']
-    last_available_year = last_available_date.split('-')[0]
 
     if month_ending:
-        temp_year = str(month_ending[0].year)
-
-        if first_available_year > temp_year or last_available_year < temp_year:
-            print('No month end data available for ' + temp_year)
-        else:
-            for i in range(1, 13):
-                rates_for_months = list(filter(lambda x: x['d'].startswith(f'{temp_year}-{i:02}'),
-                                         exchange_response['observations']))
-
-                if len(rates_for_months):
-                    _printRate(rates_for_months[-1])
-                else:
-                    break
+        _last_day_of_month_rates(month_ending, bank_of_canada_response)
     else:
-        temp_date = single_date[0].strftime('%Y-%m-%d')
-        rate_for_date = list(filter(lambda x: x['d'] == temp_date, exchange_response['observations']))
+        _specific_day_rate(kwargs['single_date'], bank_of_canada_response)
 
-        if len(rate_for_date):
-            _printRate(rate_for_date[0])
-        else:
-            print("No data available for " + temp_date)
 
-def _printRate(rate_for_date):
+def _specific_day_rate(input_date, bank_of_canada_response):
+    temp_date = input_date[0].strftime('%Y-%m-%d')
+    rate_for_date = list(filter(lambda x: x['d'] == temp_date, bank_of_canada_response['observations']))
+
+    if len(rate_for_date):
+        _print_rate(rate_for_date[0])
+    else:
+        print("No data available for " + temp_date)
+
+
+def _last_day_of_month_rates(month_ending, bank_of_canada_response):
+    year = str(month_ending[0].year)
+    first_available_year = bank_of_canada_response['observations'][0]['d'].split('-')[0]
+    last_available_year = bank_of_canada_response['observations'][-1]['d'].split('-')[0]
+
+    if first_available_year > year or last_available_year < year:
+        print('No month end data available for ' + year)
+    else:
+        for i in range(1, 13):
+            rates_for_months = list(filter(lambda x: x['d'].startswith(f'{year}-{i:02}'),
+                                           bank_of_canada_response['observations']))
+
+            if len(rates_for_months):
+                _print_rate(rates_for_months[-1])
+            else:
+                break
+
+
+def _print_rate(rate_for_date):
     rate_key = list(filter(lambda x: x != 'd', rate_for_date))[0]
     print(rate_for_date['d'], rate_for_date[rate_key]['v'])
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description
                                      ='Get CAD to USD or USD to CAD exchange rate for a specific date and last day '
                                       'of 12 months of a year.')
     parser.add_argument('start_currency', choices=('c', 'u'),
-                        help='only two options available "c" or "U".  If "c" is provided, the exchange rate will be '
+                        help='only two options available "c" or "u".  If "c" is provided, the exchange rate will be '
                              'CAD to USD.  If "u" is provided, the exchange rate will be USD to DAD')
     parser.add_argument('-m', '--annual_month_ending', nargs=1, type=lambda d: datetime.strptime(d, '%Y'),
-                        help='the year for retrieve last day of 12 months exchange rate in yyyy format')
+                        help='the year for retrieving last day of 12 months exchange rate in yyyy format')
     parser.add_argument('-d', '--single_date', nargs=1, type=lambda d: datetime.strptime(d, '%Y%m%d'),
-                        help='the date for exchange rate in yyyyMMdd format')
-    args = parser.parse_args()
+                        help='the date for retrieving exchange rate in yyyyMMdd format')
+    input_args = parser.parse_args()
 
-    if not args.annual_month_ending and not args.single_date:
-        parser.error("One of --annual_month_ending or --single_date must be given")
+    if not input_args.annual_month_ending and not input_args.single_date:
+        parser.error("One of -m or -d must be provided")
 
-    sys.exit(main(args.start_currency, month_ending=args.annual_month_ending, single_date=args.single_date))
+    sys.exit(main(input_args.start_currency, month_ending=input_args.annual_month_ending,
+                  single_date=input_args.single_date))
